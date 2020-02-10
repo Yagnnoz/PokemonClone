f8b5c7b6e23c06a5af54e4eb6e4ec79cd7b79f4a
Yagnnoz <jensk37@gmail.com>
2020-02-10 21:13:12 +0100

    finished ep 10
    clearing the screen

--- src/com/yagnnoz/realm/graphics/Screen.java
+++ src/com/yagnnoz/realm/graphics/Screen.java
@@ -23,5 +23,11 @@ public class Screen {
             }
         }
     }
+    
+    public void clear(){
+        for(int i = 0; i<pixels.length; i++){
+            pixels[i] = 0;
+        }
+    }
 
 }
