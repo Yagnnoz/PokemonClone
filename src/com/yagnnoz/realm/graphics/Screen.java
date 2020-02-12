6477e7ce6c211f36dca459cc013ed09578695a3d
Yagnnoz <jensk37@gmail.com>
2020-02-12 22:58:03 +0100

    finished ep 15
    tiles

--- src/com/yagnnoz/realm/graphics/Screen.java
+++ src/com/yagnnoz/realm/graphics/Screen.java
@@ -1,5 +1,7 @@
 package com.yagnnoz.realm.graphics;
 
+import java.util.Random;
+
 /**
  *
  * @author Jens
@@ -9,23 +11,42 @@ public class Screen {
     private int width, height;
     public int[] pixels;
 
+    public int[] tiles = new int[64 * 64];
+
+    private Random rdm = new Random();
+
     public Screen(int width, int height) {
         this.width = width;
         this.height = height;
         pixels = new int[width * height];
+
+        //fills tiles with random colors.
+        for (int i = 0; i < 64 * 64; i++) {
+            tiles[i] = rdm.nextInt(0xffffff);
+        }
     }
 
     public void render() {
         for (int y = 0; y < height; y++) {
+            if (y < 0 || y >= height) {
+                break;
+            }
+
             for (int x = 0; x < width; x++) {
-                pixels[x+y*width] = 0xff00ff;   //kein 2D array, darum so.
-                
+                if (x < 0 || x >= width) {
+                    break;
+                }
+                //x>>4 ist das gleiche wie x/16. bitshift nach rechts um  4 -> geteilt durch 2^4 => geteilt durch 16
+                //bessere performance in nested for loops
+                int tileIndex = (x >> 4) + (y >> 4) * 64;     //a single tile is 16x16
+                pixels[x + y * width] = tiles[tileIndex];   //kein 2D array, darum so.
+
             }
         }
     }
-    
-    public void clear(){
-        for(int i = 0; i<pixels.length; i++){
+
+    public void clear() {
+        for (int i = 0; i < pixels.length; i++) {
             pixels[i] = 0;
         }
     }
