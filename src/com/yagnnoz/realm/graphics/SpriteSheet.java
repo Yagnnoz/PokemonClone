e6c5af4097f194cc3e414f9ac0df641b4c32d78b
Yagnnoz <jensk37@gmail.com>
2020-02-14 23:42:28 +0100

    finished ep 20
    Sprites

--- src/com/yagnnoz/realm/graphics/SpriteSheet.java
+++ src/com/yagnnoz/realm/graphics/SpriteSheet.java
@@ -1,9 +1,35 @@
 package com.yagnnoz.realm.graphics;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
 /**
  *
  * @author Jens
  */
 public class SpriteSheet {
-    
+
+    private String path;
+    public final int SIZE;
+    public int[] pixels;
+
+    public SpriteSheet(String path, int size) {
+        this.path = path;
+        SIZE = size;
+        pixels = new int[SIZE * SIZE];
+        loadImage();
+    }
+
+    private void loadImage() {
+        try {
+            BufferedImage image = ImageIO.read(SpriteSheet.class.getResource(path));
+            int w = image.getWidth();
+            int h = image.getHeight();
+            image.getRGB(0, 0, w, h, pixels, 0, w);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
 }
