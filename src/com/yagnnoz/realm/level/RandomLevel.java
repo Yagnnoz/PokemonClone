b2dc8ed1912ab13d8c26f00fa13c1668901981b0
Yagnnoz <jensk37@gmail.com>
2020-02-16 00:56:26 +0100

    Update RandomLevel.java

--- src/com/yagnnoz/realm/level/RandomLevel.java
+++ src/com/yagnnoz/realm/level/RandomLevel.java
@@ -7,19 +7,19 @@ import java.util.Random;
  * @author Jens
  */
 public class RandomLevel extends Level {
-    
+
     private final Random random = new Random();
-    
-    public RandomLevel(int width, int height){
+
+    public RandomLevel(int width, int height) {
         super(width, height);
     }
-    
-    protected void generateLevel(){
-        for(int y = 0; y<height; y++){
-            for(int x = 0; x<width; x++){
-                tiles[x+y*width] = random.nextInt(4);
+
+    protected void generateLevel() {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                tiles[x + y * width] = random.nextInt(4);
             }
         }
     }
-    
+
 }
