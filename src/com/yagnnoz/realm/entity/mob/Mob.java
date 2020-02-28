e195e369eb79c97ef401c0c209c04ec89d07e84b
Yagnnoz <jensk37@gmail.com>
2020-02-28 20:11:01 +0100

    finished ep 42

--- src/com/yagnnoz/realm/entity/mob/Mob.java
+++ src/com/yagnnoz/realm/entity/mob/Mob.java
@@ -7,20 +7,42 @@ import com.yagnnoz.realm.graphics.Sprite;
  *
  * @author Jens
  */
-public abstract class Mob extends Entity{
-    
+public abstract class Mob extends Entity {
+
     protected Sprite sprite;
     protected int dir = 0; //direction. 0 north, 1 east, 2 south, 3 west
     protected boolean moving = false;
-    
-    public void move(){
+
+    public void move(int xa, int ya) {
+        if (xa > 0) {
+            dir = 1;
+        }
+        if (xa < 0) {
+            dir = 3;
+        }
+        if (ya > 0) {
+            dir = 2;
+        }
+        if (ya < 0) {
+            dir = 0;
+        }
+
+        if (!collision()) {
+            x += xa;
+            y += ya;
+        }
     }
-    
-    public void update(){
+
+    @Override
+    public void update() {
     }
-    
-    private boolean collision(){
+
+    public void render() {
+
+    }
+
+    private boolean collision() {
         return false;
     }
-    
+
 }
---
+++ src/com/yagnnoz/realm/entity/mob/Player.java
@@ -0,0 +1,27 @@
+package com.yagnnoz.realm.entity.mob;
+
+/**
+ *
+ * @author Jens
+ */
+public class Player extends Mob {
+
+    public Player() {
+
+    }
+
+    public Player(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    @Override
+    public void update(){
+        
+    }
+    
+    public void render(){
+        
+    }
+
+}
