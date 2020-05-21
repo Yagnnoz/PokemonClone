c7ba50d5b75184cfa8596c6dd36b80ddac03cd1a
Yagnnoz <jensk37@gmail.com>
2020-05-21 13:16:26 +0200

    Spawnsystem für Kämpfe
    finally

--- src/com/yagnnoz/realm/FightHandler.java
+++ src/com/yagnnoz/realm/FightHandler.java
@@ -1,6 +1,7 @@
 package com.yagnnoz.realm;
 
 import com.yagnnoz.realm.pokemon.Pokemon;
+import com.yagnnoz.realm.pokemon.PokemonFactory;
 
 /**
  *
@@ -11,9 +12,11 @@ public class FightHandler {
     private Pokemon opponent;
     private Pokemon ownPokemon;
     private final Game game;
+    private PokemonFactory factory;
 
     FightHandler(Game game) {
         this.game = game;
+        factory = new PokemonFactory();
     }
 
     public void setOpponent(Pokemon pkmn) {
@@ -30,7 +33,9 @@ public class FightHandler {
     }
 
     void update() {
-
+        if(opponent == null && ownPokemon == null){
+            startFight();
+        }
     }
 
     public void startFight() {
@@ -38,12 +43,19 @@ public class FightHandler {
         ownPokemon = game.getTeam().get(0);
         //figure out the enemy (wild) pokemon
         double rnd = Math.random();
-        for(int i = 0; i<game.getLevel().getSpawnrate().size()-1; i++){
+        System.out.println(rnd);
+        for(int i = 0; i<game.getLevel().getSpawnrate().size(); i++){
             if(i==0){
                 if(rnd <= game.getLevel().getSpawnrate().get(0)){
-                    
+                    opponent = factory.makePokemon(game.getLevel().getSpawns().get(i), 2);
+                }
+            }else{
+                if(rnd <= game.getLevel().getSpawnrate().get(i)  && (rnd > game.getLevel().getSpawnrate().get(i-1))){
+                    opponent = factory.makePokemon(game.getLevel().getSpawns().get(i), 2);
                 }
             }
         }
+        
+        System.out.println(opponent.toString() + " LvL: "+ opponent.getLevel()); //sollte Hornliu lvl 2 sein
     }
 }
