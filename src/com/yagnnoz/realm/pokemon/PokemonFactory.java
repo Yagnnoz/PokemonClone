777c1ca1113c989d8c401c9ba7f0318d324d8e41
Yagnnoz <jensk37@gmail.com>
2020-05-21 13:15:58 +0200

    Factory für Pokemon
    erstellt Pokemon dynamisch zur Laufzeit. Wahrscheinlich ineffizient as fuck :D

---
+++ src/com/yagnnoz/realm/pokemon/PokemonFactory.java
@@ -0,0 +1,27 @@
+package com.yagnnoz.realm.pokemon;
+
+/**
+ *
+ * @author Jens
+ */
+public class PokemonFactory {
+    
+    public Pokemon makePokemon(String name, int Lvl){
+        Pokemon result = null;
+        
+        switch (name){
+            case "Hornliu":
+                result = new Hornliu(Lvl);
+                break;
+            case "Pikachu":
+                result = new Pikachu(Lvl);
+                break;
+            case "Rattfratz":
+                result = new Rattfratz(Lvl);
+                break;
+        }
+        
+        return result;
+    }
+    
+}
