c762b5fa0bd2bef51c4748713f2068e0c6418269
Yagnnoz <jensk37@gmail.com>
2020-06-22 00:39:12 +0200

    GetMethode für reqLvl hinzugefügt

--- src/com/yagnnoz/realm/pokemon/attacken/AttackenEntry.java
+++ src/com/yagnnoz/realm/pokemon/attacken/AttackenEntry.java
@@ -5,13 +5,22 @@ package com.yagnnoz.realm.pokemon.attacken;
  * @author Jens
  */
 public class AttackenEntry {
-    
+
     public String name;
     public int reqLevel;
-    
-    public AttackenEntry(String name, int reqLvl){
+
+    public AttackenEntry(String name, int reqLvl) {
         this.name = name;
         this.reqLevel = reqLvl;
     }
-    
+
+    public int getReqLvl() {
+        if (reqLevel == 0) {
+            return 0;
+        } else {
+            return reqLevel;
+        }
+
+    }
+
 }
