771787d4858dd52386cf879a8da8f8683adaedb2
Yagnnoz <jensk37@gmail.com>
2020-06-21 22:48:36 +0200

    Typ hinzugefügt (für Schwächen, etc)

---
+++ src/com/yagnnoz/realm/pokemon/mechanics/PokemonTyp.java
@@ -0,0 +1,13 @@
+package com.yagnnoz.realm.pokemon.mechanics;
+
+/**
+ *
+ * @author Jens
+ */
+public class PokemonTyp {
+
+    public enum TYP {
+        NORMAL, FEUER, WASSER, ELEKTRO, PLANZE, KÄFER
+    };
+
+}
