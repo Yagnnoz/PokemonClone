41ab0069eda6430cc01a0cfcd46577741e516181
Yagnnoz <jensk37@gmail.com>
2022-09-26 15:49:53 +0200

    added TARGET and ATKTYPE to Enums

--- src/com/yagnnoz/realm/pokemon/mechanics/Enums.java
+++ src/com/yagnnoz/realm/pokemon/mechanics/Enums.java
@@ -1,21 +1,28 @@
 package com.yagnnoz.realm.pokemon.mechanics;
 
 /**
- *
  * @author Jens
  */
 public class Enums {
 
     public enum TYP {
-        NORMAL, FEUER, WASSER, ELEKTRO, PLANZE, KÄFER, GIFT
-    };
+        NORMAL, FEUER, WASSER, ELEKTRO, PLANZE, KAEFER, GIFT
+    }
 
     public enum PLAYERACTIONS {
         AUSWAHL, ATTACKEN, FLUCHT, WAITING
-    };
+    }
 
     public enum TURN {
         PLAYER, ENEMY
-    };
+    }
+
+    public enum TARGET {
+        SELF, ENEMY
+    }
+
+    public enum ATKTYPE {
+        PHYSICAL, SPECIAL, STATUS
+    }
 
 }
