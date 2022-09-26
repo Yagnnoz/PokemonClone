174bd03c01faf711fba5eb6281e2f5b13cac9387
Yagnnoz <jensk37@gmail.com>
2022-09-26 19:28:38 +0200

    added all current attacks to enum

--- src/com/yagnnoz/realm/pokemon/attacken/Mechanics/ANGRIFFE.java
+++ src/com/yagnnoz/realm/pokemon/attacken/Mechanics/ANGRIFFE.java
@@ -4,11 +4,18 @@ import com.yagnnoz.realm.pokemon.mechanics.Enums;
 
 import static com.yagnnoz.realm.pokemon.mechanics.Enums.ATKTYPE.*;
 import static com.yagnnoz.realm.pokemon.mechanics.Enums.TARGET.*;
+import static com.yagnnoz.realm.pokemon.mechanics.Enums.TYP.*;
+import static com.yagnnoz.realm.pokemon.mechanics.Enums.*;
 
 
 public enum ANGRIFFE {
     //Name, Target (OWN / ENEMY), STR, ACC, Typ, Type, stdPP, maxPP
-    TACKLE("Tackle", ENEMY, 30, 100, Enums.TYP.NORMAL, PHYSICAL, 15, 25);
+    TACKLE("Tackle", ENEMY, 30, 100, NORMAL, PHYSICAL, 15, 25),
+    FADENSCHUSS("Fadenschuss",ENEMY, -1,100,KAEFER, STATUS, 15, 25),
+    FEUERSTURM("Feuersturm",ENEMY, 90, 75, FEUER, PHYSICAL, 5, 8),
+    GIFTSTACHEL("Giftstachel",ENEMY, 15,95, GIFT,PHYSICAL,15,25),
+    HEULER("Heuler",ENEMY,-1,100,NORMAL, STATUS, 15,25),
+    HYDROPUMPE("Hydropumpe", ENEMY,120, 70, WASSER, PHYSICAL, 15,25);
 
     private final String NAME;
     private final Enums.TARGET TARGET;
@@ -16,8 +23,8 @@ public enum ANGRIFFE {
     private final Enums.TYP TYPE;
     private final Enums.ATKTYPE ATKTYPE;
 
-    ANGRIFFE(final String name, final Enums.TARGET target, final int str, final int acc, final Enums.TYP type,
-             final Enums.ATKTYPE atktype, final int stdPP, final int maxPP) {
+    ANGRIFFE(final String name, final TARGET target, final int str, final int acc, final TYP type,
+             final ATKTYPE atktype, final int stdPP, final int maxPP) {
         NAME = name;
         TARGET = target;
         STR = str;
